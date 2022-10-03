cde9e551d44855f76d2d666646c1d045ceb049c2
Zeus_Dxvxm <ddhabhai10@gmail.com>
2022-10-03 21:03:49 +0530

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -1,39 +1,30 @@
 import random
-play = str(input('Type NO to quit the game: '))
-guess = 1
+lower_limit = int(input("Enter the lower limit: "))
+upper = int(input('Enter the upper limit: '))
+guess = 0
 
-while (guess <= 9):
-    if play.lower() == 'no':
+while (guess < 9):
+    guess = guess + 1
+
+    n = random.randint(lower_limit, upper)
+
+    user = int(input("Enter a guess: "))
+
+    if (user < lower_limit) or (user > upper):
+        print("You exceeded the limit!\nGame Over!")
         break
+
+    if user == n:
+        print("Congratulations you have guessed the correct number!")
+        break
+
+    elif user > n:
+        print('Please guess a little lower')
+
     else:
-        pass
+        print('Please guess a little higher')
 
-        lower_limit = int(input("Enter the lower limit: "))
-        upper = int(input('Enter the upper limit: '))
-        
-        n = random.randint(lower_limit, upper)
+    print(9-guess, 'no. of guesses left')
 
-        print('You have total 9 guesses available')
-
-        user = int(input("Enter a guess: "))
-
-        if (user < lower_limit) or (user > upper):
-            print("You exceeded the limit!\nGame Over!")
-            break
-            
-        if user == n:
-            print("Congratulations you have guessed the correct number!")
-            break
-
-        elif user > n:
-            print('Please guess a little lower')
-
-        else:
-            print('Please guess a little higher')
-
-        print(9-guess, 'no. of guesses left')
-        
-        guess = guess + 1
-
-        if guess > 9:
-            print(f"You lose the number was {n}")
+    if guess >= 9:
+        print(f"You lose the number was {n}")
