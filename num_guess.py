8095fcd4b6516e6d50b9b24d0951889a8751420f
Zeus_Dxvxm <ddhabhai10@gmail.com>
2022-10-03 15:44:24 +0530

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -1,5 +1,5 @@
 import random
-play = str(input('Type YES to play the game: '))
+play = str(input('Type NO to quit the game: '))
 guess = 1
 
 while (guess <= 9):
@@ -8,17 +8,19 @@ while (guess <= 9):
     else:
         pass
 
-        lower = int(input("Enter the lower limit: "))
+        lower_limit = int(input("Enter the lower limit: "))
         upper = int(input('Enter the upper limit: '))
-        n = random.randint(lower, upper)
+        
+        n = random.randint(lower_limit, upper)
 
         print('You have total 9 guesses available')
 
         user = int(input("Enter a guess: "))
 
-        if (user < lower) or (user > upper):
+        if (user < lower_limit) or (user > upper):
             print("You exceeded the limit!\nGame Over!")
             break
+            
         if user == n:
             print("Congratulations you have guessed the correct number!")
             break
@@ -30,6 +32,7 @@ while (guess <= 9):
             print('Please guess a little higher')
 
         print(9-guess, 'no. of guesses left')
+        
         guess = guess + 1
 
         if guess > 9:
